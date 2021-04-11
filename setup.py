23233061d6495a0a1de29b4df90b411c068613bb
happyleavesaoc <happyleaves.tfr@gmail.com>
2021-04-11 14:59:26 +0000

    update packaging

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='aocref',
-    version='1.0.1',
+    version='1.0.2',
     description='Age of Empires reference data',
     url='https://github.com/siegeengineers/aoc-reference-data',
-    package_data={'': ['*']},
+    packages=find_packages(),
+    package_data={'aocref': [
+        'data/datasets/*',
+        'data/constants.json'
+    ]},
 )
