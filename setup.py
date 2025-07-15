20f0f14bbd59150ae15a2b9dbe9a94c353d51900
happyleavesaoc <happyleaves.tfr@gmail.com>
2025-07-15 00:45:00 +0000

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='2.0.24',
     description='Age of Empires reference data',
     url='https://github.com/siegeengineers/aoc-reference-data',
-    packages=find_packages(),
+    packages=find_packages(exclude=["scripts*"]),
     package_data={'aocref': [
         'data/datasets/*',
         'data/constants.json'
