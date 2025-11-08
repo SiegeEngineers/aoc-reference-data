9559c66d16cf8444b9c20ecbd5a62ba30145438d
happyleavesaoc <happyleaves.tfr@gmail.com>
2025-11-08 14:13:46 +0000

    add more civ ids

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aocref',
-    version='2.0.29',
+    version='2.0.31',
     description='Age of Empires reference data',
     url='https://github.com/siegeengineers/aoc-reference-data',
     packages=find_packages(exclude=["scripts*"]),
